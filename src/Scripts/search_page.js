1b74da16c3affe6c2b23e59c9d16d3d19e5f7cf4
cookbs <cookbs@dukes.jmu.edu>
2026-04-15 20:36:05 -0400

    Update: search function spans large inquiries

--- src/Scripts/search_page.js
+++ src/Scripts/search_page.js
@@ -16,14 +16,11 @@ var card_data;
 //call back function for query results
 setCallBackFunction(displayResults);
 
-function displayResults(data) {
+async function displayResults(data) {
 
-    if (card_data == undefined) {
-        card_data = data;
-    }
+    card_data = data;
 
     const previousCards = document.getElementsByClassName("card-result");
-    console.log(Array.from(previousCards));
 
     if (previousCards.length > 0) {
         Array.from(previousCards).forEach((card) => {
@@ -38,24 +35,51 @@ function displayResults(data) {
     // display card image and name
 
     //add support here for possible fetching of next page
-    const dataPiece = data.data.slice(curitem, curitem + pageSize);
+    if (curitem + pageSize > card_data.data.length && card_data.has_more === true) {
+
+        //get slice of data left to display
+        const cards = card_data.data;
+
+        //make fetch call ?
+        const nextData = await fetch(card_data.next_page);
+        card_data = await nextData.json();
+        console.log(card_data);
+
+        card_data.data = cards.concat(card_data.data); // combine data pieces to display
+
+    }
+
+    const dataPiece = card_data.data.slice(curitem, curitem + pageSize);
 
 
     dataPiece.forEach((card) => {
         const cardElement = template.content.cloneNode(true);
         const cardImage = cardElement.querySelector(".card-image");
         const cardName = cardElement.querySelector(".card-name");
-        if (card.card_faces != undefined) {
-            cardImage.src = card.card_faces[0].image_uris.small;
-            cardName.textContent = card.card_faces[0].name;
-        } else {
+
+        if (card.image_uris != undefined) {
             cardImage.src = card.image_uris.small;
             cardName.textContent = card.name;
-
+        } else if (card.card_faces != undefined) {
+            cardImage.src = card.card_faces[0].image_uris.small;
+            cardName.textContent = card.card_faces[0].name;
         }
         results.appendChild(cardElement);
         //add event listener to card to display card details and ability to add to deck
+
     });
+
+    if (curitem - pageSize >= 0) { // if there are previous results to show, show scroll left button
+        document.getElementById("scroll-left").classList.remove("hidden");
+    } else if (curitem - pageSize < 0) { // if there are no previous results to show, hide scroll left button
+        document.getElementById("scroll-left").classList.add("hidden");
+    }
+
+    if (curitem + pageSize < card_data.data.length) { // if there are more results to show, show scroll right button
+        document.getElementById("scroll-right").classList.remove("hidden");
+    } else if (curitem + pageSize >= card_data.data.length) { // if there are no more results to show, hide scroll right button
+        document.getElementById("scroll-right").classList.add("hidden");
+    }
 }
 
 //add ability to load more results when user presses a button to go forward/back through results
